9c5c19d2c7825e8decc2c3c72ed658ad8ae62ac6
soju <sojucr7@gmail.com>
2022-09-26 15:45:31 +0530

    file renamed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import GoogleCalendar from './components/GoogleCalendar';
+import Calendar from './components/Calendar';
 import CalendarHeader from './components/CalendarHeader';
 import CalendarSidebar from './components/CalendarSidebar';
 function App() {
   return (
     <div className="App">
         <CalendarHeader/>
-
         <div className='content'>
             <CalendarSidebar/>
-            <GoogleCalendar/>
-        </div>
-        
+            <Calendar/>
+        </div> 
     </div>
   );
 }
-
 export default App;
--- src/components/CalendarSidebar.tsx
+++ src/components/CalendarSidebar.tsx
@@ -3,14 +3,6 @@ function CalendarSidebar(){
 
     return (
         <div className='calendar-sidebar'>
-            <button>TODO</button>
-
-             <div className='logo'>Calendar</div>
-
-             <div className='navigation-container'>
-                <span className="prev">&lt;</span>
-                <span className="next">&gt;</span>
-             </div>
         </div>
     )
 
